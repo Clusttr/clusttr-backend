a482d76b0ce8be7a1a4db64cd47412768de4dd0e
Chukwuemeka Matthew <matthew.chukwuemeka40@gmail.com>
2023-10-11 19:44:44 +0100

    create provision for hexadecimal private key

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -4,7 +4,7 @@ import { Keypair } from '@solana/web3.js';
 import { TransactionResponseDto } from './dto/transaction-response.dto';
 import { RegisterAccountDto } from './dto/register-account.dto';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { generateAccount } from 'src/solana/utils/get-account';
+import { generateAccount, generateHexAccount } from 'src/solana/utils/get-account';
 import { JwtAuthGuard } from 'src/middlewere/jwt.guard';
 
 @Controller('account')
@@ -19,6 +19,17 @@ export class AccountController {
     @ApiResponse({status: 201, description: "Register use keypair on solana chain", type: TransactionResponseDto})
     async register(@Body() req: RegisterAccountDto): Promise<TransactionResponseDto> {
         const keypair =  generateAccount(req.secret)
+        const signature = await this.accountService.registerAccount(keypair)
+
+        return { success: true, signature}
+    }
+
+    @Post("/registerhex")
+    @ApiBody({type: RegisterAccountDto})
+    @ApiOperation({ summary: "Login Account"})
+    @ApiResponse({status: 201, description: "Register use keypair on solana chain", type: TransactionResponseDto})
+    async registerHex(@Body() req: RegisterAccountDto): Promise<TransactionResponseDto> {
+        const keypair =  generateHexAccount(req.secret)
         const signature = await this.accountService.registerAccount(keypair)
 
         return { success: true, signature}
--- src/solana/utils/get-account.ts
+++ src/solana/utils/get-account.ts
@@ -5,3 +5,8 @@ const bs58 = require('bs58')
 export const generateAccount = (secretKey: string) => {
     return Keypair.fromSecretKey(bs58.decode(secretKey))
 }
+
+export const generateHexAccount = (secretKey: string) => {
+    const secretKeyBinary = Uint8Array.from(Buffer.from(secretKey, 'hex'))
+    return Keypair.fromSecretKey(secretKeyBinary)
+}
